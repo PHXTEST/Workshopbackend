a6d802977af59471bc3f15246577b1742a591e89
Thomas <107029185+PHXTEST@users.noreply.github.com>
2023-10-03 13:13:50 +0200

    Add files via upload

---
+++ workshop2023/src/main/java/com/backend/workshop2023/Workshop2023Application.java
@@ -0,0 +1,13 @@
+package com.backend.workshop2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop2023Application.class, args);
+	}
+
+}
---
+++ workshop2023/src/test/java/com/backend/workshop2023/Workshop2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.workshop2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshop2023ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
